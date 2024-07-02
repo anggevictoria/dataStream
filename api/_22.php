7cd4f9736fada88debff82bdf7eb79aa3182f06d
angge <166150697+anggevictoria@users.noreply.github.com>
2024-07-03 00:21:25 +0800

    init

---
+++ api/_22.php
@@ -0,0 +1,12 @@
+<?php
+include '../data.php';
+
+header('Content-Type: application/json');
+
+$allData = getEVData();
+$filteredData = array_filter($allData, function($item) {
+    return strpos($item['date'], '2022') !== false;
+});
+
+echo json_encode(array_values($filteredData));
+?>
